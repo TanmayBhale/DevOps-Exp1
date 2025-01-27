72b20c62fe50fbd9d04d1b5d3913f2f6cb8da8b4
TanmayBhale <tanmaybhale1@gmail.com>
2025-01-27 21:49:06 +0530

    Modified line no 5

--- PersonalDetails.java
+++ PersonalDetails.java
@@ -2,7 +2,7 @@ public class PersonalDetails {
     public static void main(String[] args) {
         // Personal Details
         System.out.println("Name: Tanmay Bhale");
-        System.out.println("Email: tanmaybhale1@gmail.com");
+        System.out.println("Email: ttbhale_m24@ce.vjti.ac.in");
         System.out.println("GitHub Username: TANMAY BHALE");
         System.out.println("Program: Demonstrates Git Push Example");
         
