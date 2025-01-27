7d2f31e2426a64a613a5b2795427d940fe0b00da
TanmayBhale <tanmaybhale1@gmail.com>
2025-01-27 21:44:41 +0530

    Initial Commit

---
+++ PersonalDetails.java
@@ -0,0 +1,10 @@
+public class PersonalDetails {
+    public static void main(String[] args) {
+        // Personal Details
+        System.out.println("Name: Tanmay Bhale");
+        System.out.println("Email: tanmaybhale1@gmail.com");
+        System.out.println("GitHub Username: TANMAY BHALE");
+        System.out.println("Program: Demonstrates Git Push Example");
+        
+    }
+}
